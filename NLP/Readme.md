9a2017d97f9064135585f87d37156c8a4738c965
guzele <giselegarifullina@gmail.com>
2018-01-24 22:01:58 +0300

    Saved trained model

--- NLP/Readme.md
+++ NLP/Readme.md
@@ -12,14 +12,19 @@ to create them run data_extractor.py
 
 dnn.py - used to train and test model
 
-Dataset created by https://github.com/AnnaAK.
+model.json
+model.h5  - saved model
 
-acc: 97.70%
+Dataset created by https://github.com/AnnaAK and used sources: http://wiki.dbpedia.org/Downloads2014?v=jd3, http://ftp.acc.umu.se/mirror/wikimedia.org/dumps/skwiki/20180101/, http://zlatyfond.sme.sk/autori
 
+
+Total time used for training: 0:23:02.020427
+acc: 97.67%
              precision    recall  f1-score   support
 
-         cs       0.95      0.98      0.97     62576
-         hu       1.00      1.00      1.00     50062
-         pl       1.00      1.00      1.00     50130
-         sk       0.96      0.92      0.94     37232
+         cs       0.95      0.98      0.96     62576
+         pl       1.00      1.00      1.00     50062
+         sk       1.00      1.00      1.00     50130
+         hu       0.96      0.91      0.94     37232
 
+avg / total       0.98      0.98      0.98    200000
--- NLP/dnn.py
+++ NLP/dnn.py
@@ -2,7 +2,8 @@ import general as g
 from alphabet import define_alphabet, decode_langid
 from data_extractor import get_train_test_data, get_feature
 
-
+import time
+import datetime
 import matplotlib
 import matplotlib.pyplot as plt
 from sklearn import metrics
@@ -14,6 +15,9 @@ from keras.layers import Dropout
 import keras.optimizers
 from keras.utils import plot_model
 import keras.utils
+from keras.models import model_from_json
+from keras.models import load_model as load
+
 
 
 
@@ -32,18 +36,49 @@ def createModel (input_size):
               metrics=['accuracy'])
     return model
 
-def main():
-    alphabet = define_alphabet()
-    input_size = len(get_feature('a', alphabet))
-
-    (X_train, Y_train, X_test, Y_test) = get_train_test_data(input_size)
+def train_save_model(input_size, X_train, Y_train, X_test, Y_test):
     model = createModel(input_size)
+    start_time = time.time()
     history = model.fit(X_train,Y_train,
           epochs= 12, #12
           validation_split=0.10,
           batch_size=64,
           verbose=2,
           shuffle=True)
+    finish_time = time.time()
+    print ("Learning took " + str(datetime.timedelta(seconds=finish_time - start_time)))
+   
+    # serialize model to JSON
+    model_json = model.to_json()
+    with open("model.json", "w") as json_file:
+       json_file.write(model_json)
+     # serialize weights to HDF5
+    model.save_weights("model.h5")
+    print("Saved model to disk")
+
+    return model
+
+def load_model():
+    json_file = open('model.json', 'r')
+    loaded_model_json = json_file.read()
+    json_file.close()
+    loaded_model = model_from_json(loaded_model_json)
+    # load weights into new model
+    loaded_model.load_weights("model.h5")
+    print("Loaded model from disk")
+    return loaded_model
+
+def main():
+    alphabet = define_alphabet()
+    input_size = len(get_feature('a', alphabet))
+
+    (X_train, Y_train, X_test, Y_test) = get_train_test_data(input_size)
+    #model = train_save_model(input_size, X_train, Y_train, X_test, Y_test)
+    model = load_model()
+    
+    model.compile(optimizer='rmsprop',
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
     # now we will face the TRUTH. What is our model real accuracy tested on unseen data?
     scores = model.evaluate(X_test, Y_test, verbose=1)
     print("%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
